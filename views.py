38f34c14a08267f96386911144421a3136183506
VaradDeolankar <varaddeolankar@gmail.com>
2016-02-24 01:22:08 +0530

    C'mon

--- views.py
+++ views.py
@@ -30,8 +30,37 @@ def index():
             print row
             
         conn.close();
-        return render_template('success.html',
+        return render_template('index.html',
                                 data=rows,
                                 error=error)
     except:
         return "Failed :/"
+
+        
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+    try:
+        conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
+
+        error=None
+        cur = conn.cursor()
+        cur.execute("""SELECT fname, lname, age FROM Users WHERE email=""" + request.form['email'] + """AND password=""" + request.form['password'] + """;""")
+        rows = cur.fetchall();
+        conn.close()
+        iflen(rows)) >= 1:
+            return render_template('success.html')
+    
+    except:
+        return "Failed :/"
+        
+        
+        
